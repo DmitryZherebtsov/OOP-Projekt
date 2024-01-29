e1bcca598cd5422a6f138b5059d9de4fcb3de21d
Dmity <zherebtsovdima31@gmail.com>
2024-01-29 13:55:33 +0100

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+        int n, i;
+        n = 10;
+        i = 1;
+        System.out.println(n-i);
     }
 }
